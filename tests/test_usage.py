6e1401fdd0ec26059c4b66f0f262518e9c815e3a
Amos Jun-yeung Ng <me@amos.ng>
2023-05-30 15:05:22 +1000

    Upgrade to latest langchain

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -11,21 +11,21 @@ def test_use_as_with_context() -> None:
     cassette_path = "tests/context.yaml"
     with TemporaryCassettePath(cassette_path):
         with vcr.use_cassette(cassette_path, record_mode=vcr.mode.ONCE):
-            llm = OpenAI()
+            llm = OpenAI(model_name="text-ada-001")
             result = llm("Tell me a silly joke")
 
         with vcr.use_cassette(cassette_path, record_mode=vcr.mode.NONE):
-            new_llm = OpenAI()
+            new_llm = OpenAI(model_name="text-ada-001")
             assert new_llm("Tell me a silly joke") == result
 
 
 @vcr.use_cassette()
 def test_use_as_test_decorator() -> None:
-    llm = OpenAI()
+    llm = OpenAI(model_name="text-ada-001")
     llm("Tell me a surreal joke")
 
 
 @vcr.use_cassette("tests/custom.yaml")
 def test_use_custom_file() -> None:
-    llm = OpenAI()
+    llm = OpenAI(model_name="text-ada-001")
     llm("Tell me a real joke")
